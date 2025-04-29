bb788c0de3b54e8fc2096adc6b7dd5c397aab43c
Oriol <oroig@institutjaumehuguet.cat>
2025-04-29 21:07:23 +0200

    Afegit llegirInt a InputHelper

--- ProjecteEntornsXavi/src/main/java/botiga/Utilitats/InputHelper.java
+++ ProjecteEntornsXavi/src/main/java/botiga/Utilitats/InputHelper.java
@@ -1,4 +1,21 @@
 package botiga.Utilitats;
 
 public class InputHelper {
+    public static int llegirInt() {
+        Scanner scanner = new Scanner(System.in);
+        int nombre = 0;
+        boolean valid = false;
+
+        while (!valid) {
+            try {
+                System.out.print("Introdueix el nombre: ");
+                nombre = Integer.parseInt(scanner.nextLine());
+                valid = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Error.Nombre no valid. Torna a intentar-ho: ");
+            }
+        }
+        return nombre;
+    }
 }
