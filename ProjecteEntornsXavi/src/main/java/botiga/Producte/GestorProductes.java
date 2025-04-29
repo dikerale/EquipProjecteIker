e268ca5fb4030e42c5fb97198cffa5143461a507
abosch <abosch32@institutjaumehuguet.cat>
2025-04-29 21:09:06 +0200

    gestorProductes-ari-guillem(en procés)

--- ProjecteEntornsXavi/src/main/java/botiga/Producte/GestorProductes.java
+++ ProjecteEntornsXavi/src/main/java/botiga/Producte/GestorProductes.java
@@ -1,4 +1,37 @@
 package botiga.Producte;
+import java.util.Scanner;
 
 public class GestorProductes {
+    Scanner sc = new Scanner(System.in);
+
+    public void afegirProducte(Producte producte){
+        return ;
+        //afegir producte a on?
+    }
+
+    public void crearProducte(){
+        System.out.println("Introdueix el nom del producte: ");
+        String nom = sc.nextLine();
+
+        System.out.println("Introdueix el preu del producte: ");
+        double preu = sc.nextDouble();
+
+        System.out.println("Introdueix l'stock del producte: ");
+        sc.nextInt();
+        int stock = sc.nextInt();
+        Producte p = new Producte(nom,preu,stock);
+
+        System.out.println("Producte creat correctament");
+    }
+
+    public void eliminarProdute(){
+        return;
+        //eliminar producte d'on?
+
+    }
+
+    public void aplicarDescomptes(Producte producte, double descompte){
+        producte.setPreu(producte.getPreu()-descompte);
+    }
+
 }
