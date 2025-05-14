0092b1873486b1114098f2ba8bedc5d031660ba6
gpolancoo <gpolanco@institutjaumehuguet.cat>
2025-05-14 16:40:38 +0200

    PaquetVenda-test-corregit-genesis

--- ProjecteEntornsXavi/src/test/java/botiga/botiga/VentaTest.java
+++ ProjecteEntornsXavi/src/test/java/botiga/botiga/VentaTest.java
@@ -5,14 +5,21 @@ import botiga.Usuaris.Usuari;
 import botiga.Venda.Venda;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
 
 public class VentaTest {
-
     @Test
     public void testConstructor() {
-        Usuari usuari = new Usuari("Joan", "joan@gmail.com", "client");
-        Venda venda = new Venda(usuari);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introdueix el nom de l'usuari: ");
+        String nom = scan.nextLine();
+        System.out.println("Introdueix el correu de l'usuari: ");
+        String correu = scan.nextLine();
+        System.out.println("Introdueix el rol de l'usuari: ");
+        String rol = scan.nextLine();
+        Usuari usuari = new Usuari(nom, correu, rol);
 
+        Venda venda = new Venda(usuari);
         assertEquals(usuari, venda.getUsuari());
         assertNotNull(venda.getData());
         assertEquals(0.0, venda.getTotal());
@@ -21,11 +28,27 @@ public class VentaTest {
 
     @Test
     public void testAfegirLiniaIVerificarTotal() {
-        Usuari usuari = new Usuari("Joan", "joan@gmail.com", "client");
-        Producte producte = new Producte("Goma", 1.0, 200);
-        Venda venda = new Venda(usuari);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introdueix el nom de l'usuari: ");
+        String nom = scan.nextLine();
+        System.out.println("Introdueix el correu de l'usuari: ");
+        String correu = scan.nextLine();
+        System.out.println("Introdueix el rol de l'usuari: ");
+        String rol = scan.nextLine();
+        Usuari usuari = new Usuari(nom, correu, rol);
 
-        venda.afegirLinia(producte, 3);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introdueix el nom del prducte: ");
+        String nomP = sc.nextLine();
+        System.out.println("Introdueix preu de el producte de l'usuari: ");
+        double preuP = sc.nextInt();
+        System.out.println("Introdueix l'estoc de el producte: ");
+        int stock = sc.nextInt();
+        Producte producte = new Producte(nomP, preuP, stock );
+        Venda venda = new Venda(usuari);
+        System.out.println("Introdueix la quantitat de producte que vols: ");
+        int quantitat = scan.nextInt();
+        venda.afegirLinia(producte, quantitat);
 
         assertEquals(1, venda.getLinies().size());
         assertEquals(3.0, venda.getTotal(), 0.001);
@@ -33,10 +56,29 @@ public class VentaTest {
 
     @Test
     public void testToStringContingut() {
-        Usuari usuari = new Usuari("Maria", "maria@gmail.com", "client");
-        Producte producte = new Producte("Carpeta", 5.0, 50);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introdueix el nom de l'usuari: ");
+        String nom = scan.nextLine();
+        System.out.println("Introdueix el correu de l'usuari: ");
+        String correu = scan.nextLine();
+        System.out.println("Introdueix el rol de l'usuari: ");
+        String rol = scan.nextLine();
+        Usuari usuari = new Usuari(nom, correu, rol);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introdueix el nom del prducte: ");
+        String nomP = sc.nextLine();
+        System.out.println("Introdueix preu de el producte de l'usuari: ");
+        double preuP = sc.nextInt();
+        System.out.println("Introdueix l'estoc de el producte: ");
+        int stock = sc.nextInt();
+        Producte producte = new Producte(nomP, preuP, stock);
+
         Venda venda = new Venda(usuari);
-        venda.afegirLinia(producte, 2); // 10€
+        Scanner s = new Scanner(System.in);
+        System.out.println("Introdueix la quantitat de producte que vols: ");
+        int quantitat = scan.nextInt();
+        venda.afegirLinia(producte, quantitat);
 
         String resultat = venda.toString();
         assertTrue(resultat.contains("Carpeta"));
