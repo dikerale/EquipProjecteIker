e4e52de9665cb186f2619fd5d5c186363c44568f
LLUC\llucs <llucsegala29@gmail.com>
2025-05-14 19:40:10 +0100

    Nuevos cambios

--- ProjecteEntornsXavi/src/test/java/botiga/botiga/GestorUsuariTest.java
+++ ProjecteEntornsXavi/src/test/java/botiga/botiga/GestorUsuariTest.java
@@ -1,4 +1,44 @@
-package botiga;
+package botiga.botiga;
+
+import botiga.Usuaris.GestioUsuari;
+import botiga.Usuaris.Usuari;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GestorUsuariTest {
+
+    @Test
+    public void testAfegirUsuari() {
+        GestioUsuari gestor = new GestioUsuari();
+        Usuari usuari = new Usuari("Lluc", "lluc@gmail.com", "CLIENT");
+
+        gestor.afegirUsuari(usuari);
+
+        assertTrue(gestor.validarExistencia("lluc@gmail.com"));
+    }
+
+    @Test
+    public void testObtenirUsuariPerNom() {
+        GestioUsuari gestor = new GestioUsuari();
+        Usuari usuari = new Usuari("Nami", "nami@gmail.com", "ADMINISTRADOR");
+        gestor.afegirUsuari(usuari);
+
+        Usuari resultat = gestor.obtenirUsuariPerNom("nami");
+
+        assertNotNull(resultat);
+        assertEquals("nami@gmail.com", resultat.getCorreuElectronic());
+        assertEquals("ADMINISTRADOR", resultat.getRol());
+    }
+
+    @Test
+    public void testValidarExistencia() {
+        GestioUsuari gestor = new GestioUsuari();
+        Usuari usuari = new Usuari("Monday", "monday@botiga.com", "CLIENT");
+        gestor.afegirUsuari(usuari);
+
+        assertTrue(gestor.validarExistencia("monday@botiga.com"));
+        assertFalse(gestor.validarExistencia("sensecorreu@botiga.com"));
+    }
 }
+
--- ProjecteEntornsXavi/src/test/java/botiga/botiga/UsuariTest.java
+++ ProjecteEntornsXavi/src/test/java/botiga/botiga/UsuariTest.java
@@ -1,4 +1,40 @@
-package botiga;
+package botiga.botiga;
+
+import botiga.Usuaris.Usuari;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UsuariTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+        Usuari usuari = new Usuari("Lluc", "lluc@gmail.com", "CLIENT");
+
+        assertEquals("Lluc", usuari.getNom());
+        assertEquals("lluc@gmail.com", usuari.getCorreuElectronic());
+        assertEquals("CLIENT", usuari.getRol());
+    }
+
+    @Test
+    public void testSetters() {
+        Usuari usuari = new Usuari("NomAntic", "antic@exemple.com", "CLIENT");
+
+        usuari.setNom("NomNou");
+        usuari.setCorreu("nou@exemple.com");
+        usuari.setRol("ADMINISTRADOR");
+
+        assertEquals("NomNou", usuari.getNom());
+        assertEquals("nou@exemple.com", usuari.getCorreuElectronic());
+        assertEquals("ADMINISTRADOR", usuari.getRol());
+    }
+
+    @Test
+    public void testToString() {
+        Usuari usuari = new Usuari("Nami", "nami@correu.com", "ADMINISTRADOR");
+
+        String expected = "Nom: Nami\nCorreu: nami@correu.com\nRol: ADMINISTRADOR";
+        assertEquals(expected, usuari.toString());
+    }
 }
+
