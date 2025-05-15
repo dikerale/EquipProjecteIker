6cb416b33f94604d6d98a19e4d53d8f42a9e65e5
abosch <abosch32@institutjaumehuguet.cat>
2025-05-15 17:55:14 +0200

    MAIN1

--- ProjecteEntornsXavi/src/main/java/botiga/Main.java
+++ ProjecteEntornsXavi/src/main/java/botiga/Main.java
@@ -1,4 +1,108 @@
 package botiga;
 
+import botiga.Producte.GestorProductes;
+import botiga.Producte.Producte;
+import botiga.Usuaris.GestioUsuari;
+import botiga.Usuaris.Usuari;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------\n\t  --Benvingut al sistema--\n--------------------------------------");
+        Scanner sc = new Scanner(System.in);
+
+        //menu
+        while (true) {
+            try {
+                GestorProductes gestorProductes = new GestorProductes();
+                GestioUsuari gestioUsuari = new GestioUsuari();
+                //creació productes inicials
+                gestorProductes.afegirProducte("pilota blanca", 10, 50);
+                gestorProductes.afegirProducte("pilota vermella", 12, 25);
+                gestorProductes.afegirProducte("pilota colors", 16, 46);
+                gestorProductes.afegirProducte("samarreta Mesi", 60, 105);
+
+                System.out.println("| Menu |\n1-Productes\n2-Usuaris\n3-Venda\n4-Sortir del programa");
+                int menu1 = sc.nextInt();
+                if (menu1 == 1) {
+                    while (true) {
+                        try {
+                            System.out.println("| Productes |\n1-Llistar productes\n2-Cercar producte\n3-Afegir producte\n4-Eliminar producte\n5-Aplicar descompte\n6-Sortir al menu principal");
+                            int menu2 = sc.nextInt();
+                            if (menu2 == 1) {
+                                System.out.println("---------");
+                                gestorProductes.llistatProductes();
+                                System.out.println("---------");
+
+                            } else if (menu2 == 2) {
+                                System.out.println("---------");
+                                gestorProductes.cercarProducte();
+                                System.out.println("---------");
+
+                            } else if (menu2 == 3) {
+                                System.out.println("---------");
+                                gestorProductes.afegirProducte();
+                                System.out.println("---------");
+
+                            } else if (menu2 == 4) {
+                                System.out.println("---------");
+                                gestorProductes.eliminarProdute();
+                                System.out.println("---------");
+
+                            } else if (menu2 == 5) {
+                                System.out.println("---------");
+                                gestorProductes.aplicarDescompte();
+                                System.out.println("---------");
+
+                            } else if (menu2 == 6) {
+                                System.out.println("---------");
+                                break;
+                            } else {
+                                System.out.println("No has introduit un nombre correcte");
+                            }
+                        } catch (InputMismatchException IME) {
+                            System.out.println("Caràcter incorrecte");
+                            sc.nextLine();
+                        } catch (Exception e) {
+                            System.out.println("S'ha produït un error inesperat: " + e.getMessage());
+                        }
+                    }
+                } else if (menu1 == 2) {
+                    System.out.println("| Usuaris |\n1-Afegir un usuari\n2-Cercar un usuari\n3-Validar existencia\n4-Sortir al menu principal");
+                    int menu2 = sc.nextInt();
+                    sc.nextLine();
+                    if (menu2 == 1) {
+                        System.out.println("---------");
+                        System.out.println("Introdueix el nom de l'Usuari: ");
+                        String nom = sc.nextLine();
+                        System.out.println("Introdueix el correu de l'Usuari: ");
+                        String correu = sc.nextLine();
+                        System.out.println("Introdueix el rol de l'Usuari: ");
+                        String rol = sc.nextLine();
+
+                        Usuari usuari = new Usuari(nom, correu, rol);
+                        gestioUsuari.afegirUsuari(usuari);
+                        System.out.println("---------");
+
+                    }
+                } else if (menu1 == 3) {
+                    System.out.println("");
+                } else if (menu1 == 4) {
+                    System.out.println("Sortint del programa...");
+                    break;
+                } else {
+                    System.out.println("No has introduit un nombre correcte");
+                }
+            }catch (InputMismatchException ime) {
+                System.out.println("Caràcter incorrecte");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("S'ha produït un error inesperat: " + e.getMessage());
+            }
+        }
+    }
+
 }
--- ProjecteEntornsXavi/src/main/java/botiga/Producte/GestorProductes.java
+++ ProjecteEntornsXavi/src/main/java/botiga/Producte/GestorProductes.java
@@ -1,6 +1,8 @@
 package botiga.Producte;
 import botiga.Producte.Producte;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class GestorProductes {
@@ -8,86 +10,150 @@ public class GestorProductes {
     public ArrayList<Producte> llistaProductes = new ArrayList<>();
 
 
-    //CREACIÓ D'UN PRODUCTE NOU
-    public void afegirProducte() {
-        System.out.println("Introdueix el nom del producte: ");
-        String nom = sc.nextLine();
-
-        System.out.println("Introdueix el preu del producte: ");
-        double preu = sc.nextDouble();
-
-        System.out.println("Introdueix l'estoc del producte: ");
-        sc.nextInt();
-        int stock = sc.nextInt();
-        Producte p = new Producte(nom,preu,stock);
-
-        System.out.println("Producte creat correctament.");
-        llistaProductes.add(p);
-    }
-
-    public void cercarProducte() {
-        System.out.println("Introdueix el nom del producte: ");
-        String nom = sc.nextLine();
-
+    //Llistar productes
+    public void llistatProductes() {
         if (llistaProductes.isEmpty()) {
-            System.out.println("La llista està buida.");
+            System.out.println("La llista està buida");
         } else {
-            Boolean ProducteCercat = Boolean.FALSE;
-            for (Producte p : llistaProductes) {
-                if (p.getNom().equals(nom)) {
-                    System.out.println(p);
-                    ProducteCercat = Boolean.TRUE;
-                }
+            for (Producte producte : llistaProductes) {
+                System.out.println(producte);
+                System.out.println("___");
             }
         }
     }
 
+    //CREACIÓ D'UN PRODUCTE NOU
+    public void afegirProducte() {
+        try {
+            System.out.println("Introdueix el nom del producte: ");
+            String nom = sc.nextLine();
+
+            System.out.println("Introdueix el preu del producte: ");
+            double preu = sc.nextDouble();
+
+            System.out.println("Introdueix l'estoc del producte: ");
+            int stock = sc.nextInt();
+            sc.nextLine();
+            Producte p = new Producte(nom,preu,stock);
+
+            System.out.println("Producte creat correctament.");
+            llistaProductes.add(p);
+        }catch (InputMismatchException ime) {
+            System.out.println("Caràcter incorrecte");
+            sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("S'ha produït un error inesperat: " + e.getMessage());
+        }
+    }
+    //creació productes desdel codi
+    public void afegirProducte(String nom, double preu, int stock) {
+        Producte p = new Producte(nom,preu,stock);
+        llistaProductes.add(p);
+    }
+
+    //Cercar un producte
+    public void cercarProducte() {
+        try {
+            System.out.println("Introdueix el nom del producte: ");
+            String nom = sc.nextLine().toLowerCase();
+            Boolean ProducteCercat = Boolean.FALSE;
+
+            if (llistaProductes.isEmpty()) {
+                System.out.println("La llista està buida.");
+            } else {
+                for (Producte p : llistaProductes) {
+                    if (p.getNom().toLowerCase().startsWith(nom)) {
+                        System.out.println(p);
+                        System.out.println("___");
+                        ProducteCercat = Boolean.TRUE;
+                    }
+                }
+                if (ProducteCercat == Boolean.FALSE) {
+                    System.out.println("No s'ha trobat el producte buscat");
+                }
+            }
+        }catch (InputMismatchException ime) {
+            System.out.println("Caràcter incorrecte");
+            sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("S'ha produït un error inesperat: " + e.getMessage());
+        }
+    }
+
+    //eliminar un producte
     public void eliminarProdute() {
-        System.out.println("Introdueix el nom del producte: ");
-        String nom = sc.nextLine();
+        try {
+            System.out.println("Introdueix el nom del producte: ");
+            String nom = sc.nextLine();
 
-        if (llistaProductes.isEmpty()) {
-            System.out.println("La llista està buida.");
-        } else {
+            if (llistaProductes.isEmpty()) {
+                System.out.println("La llista està buida.");
+            } else {
+                Boolean ProducteCercat = Boolean.FALSE;
+                for (Producte p : llistaProductes) {
+                    if (p.getNom().equals(nom)) {
+                        System.out.println("Vols eliminar aquest producte? 1. Sí - 2. No");
+                        System.out.println(p);
+                        ProducteCercat = Boolean.TRUE;
+                        int confirmacio = sc.nextInt();
+                        if (confirmacio == 1) {
+                            llistaProductes.remove(p);
+                            System.out.println("S'ha eliminat el producte.");
+                            break;
+                        } else if (confirmacio == 2) {
+                            System.out.println("No s'ha eliminat el producte.");
+                        } else {
+                            System.out.println("ERROR - No s'ha introduit un nombre vàlid");
+                        }
+                    }
+                }
+                if (ProducteCercat == Boolean.FALSE) {
+                    System.out.println("No s'ha trobat el producte buscat");
+                }
+            }
+        }catch (InputMismatchException ime) {
+            System.out.println("Caràcter incorrecte");
+            sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("S'ha produït un error inesperat: " + e.getMessage());
+        }
+    }
 
+    //aplicar descompte a un producte
+    public void aplicarDescompte() {
+        try {
+            System.out.println("Introdueix el nom del producte: ");
+            String nom = sc.nextLine();
+            System.out.println("Introdueix el descompte a aplicar: ");
+            double descompte = sc.nextDouble();
+            sc.nextLine();
+            Boolean ProducteCercat = Boolean.FALSE;
             for (Producte p : llistaProductes) {
                 if (p.getNom().equals(nom)) {
-                    System.out.println("Vols eliminar aquest producte? 1. Sí - 2. No");
+                    System.out.println("Vols modificar el preu d'aquest producte? 1. Sí - 2. No");
                     System.out.println(p);
+                    ProducteCercat = Boolean.TRUE;
                     int confirmacio = sc.nextInt();
+                    sc.nextLine();
                     if (confirmacio == 1) {
-                        llistaProductes.remove(p);
-                        System.out.println("S'ha eliminat el producte.");
-                    } else  if (confirmacio == 2) {
-                        System.out.println("No s'ha eliminat el producte.");
+                        p.setPreu(p.getPreu() * (1 - (descompte / 100)));
+                        System.out.println("Descompte aplicat");
+                        System.out.println(p);
+                    } else if (confirmacio == 2) {
+                        System.out.println("No s'ha aplicat cap descompte");
                     } else {
                         System.out.println("ERROR - No s'ha introduit un nombre vàlid");
                     }
                 }
             }
-        }
-
-    }
-    public void aplicarDescomptes() {
-        System.out.println("Introdueix el nom del producte: ");
-        String nom = sc.nextLine();
-        System.out.println("Introdueix el descompte a aplicar: ");
-        double descompte = sc.nextDouble();
-
-        for (Producte p : llistaProductes) {
-            if (p.getNom().equals(nom)) {
-                System.out.println("Vols modificar el preu d'aquest producte? 1. Sí - 2. No");
-                System.out.println(p);
-                int confirmacio = sc.nextInt();
-                if (confirmacio == 1) {
-                    p.setPreu(p.getPreu()-descompte);
-                    System.out.println("Descompte aplicat");
-                } else  if (confirmacio == 2) {
-                    System.out.println("No s'ha aplicat cap descompte");
-                } else {
-                    System.out.println("ERROR - No s'ha introduit un nombre vàlid");
-                }
+            if (ProducteCercat == Boolean.FALSE) {
+                System.out.println("No s'ha trobat el producte buscat");
             }
+        }catch (InputMismatchException ime) {
+            System.out.println("Caràcter incorrecte");
+            sc.nextLine();
+        } catch (Exception e) {
+            System.out.println("S'ha produït un error inesperat: " + e.getMessage());
         }
     }
 }
